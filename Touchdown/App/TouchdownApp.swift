c2c360b959cd14db7c4e0fb2c0078402eead32e1
Arthur Neves <arthurnvsm@gmail.com>
2021-11-29 09:14:09 -0300

    Project first settings

---
+++ Touchdown/App/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Arthur Neves on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Arthur Neves on 29/11/21.
+//
+
+import SwiftUI
+
+//DATA
+//COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+//LAYOUT
+//UX
+//API
+//IMAGE
+//FONT
+//STRING
+//ETC.
