b93cdaf9d49f817d7c56d7b9f9c4cdb59a120a4c
Arthur Neves <arthurnvsm@gmail.com>
2021-11-30 14:40:22 -0300

    Logo animation

--- Touchdown/View/Home/NavigationBarView.swift
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct NavigationBarView: View {
+  
+  @State private var isAnimated = false
+  
   var body: some View {
     HStack {
       Button(action: {}) {
@@ -15,6 +18,17 @@ struct NavigationBarView: View {
           .font(.title)
           .foregroundColor(.black)
       } //: Button
+      
+      Spacer()
+      
+      LogoView()
+        .opacity(isAnimated ? 1 : 0)
+        .offset(x: 0, y: isAnimated ? 0 : -25)
+        .onAppear(perform: {
+          withAnimation(.easeOut(duration: 0.5)) {
+            isAnimated.toggle()
+          }
+        })
       
       Spacer()
       
