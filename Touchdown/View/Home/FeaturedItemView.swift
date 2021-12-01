69e041f88e6b8f67d65645633fffc76a90a9b64a
Arthur Neves <arthurnvsm@gmail.com>
2021-12-01 07:34:42 -0300

    TabView implementation

---
+++ Touchdown/View/Home/FeaturedItemView.swift
@@ -0,0 +1,28 @@
+//
+//  FeaturedItemView.swift
+//  Touchdown
+//
+//  Created by Arthur Neves on 01/12/21.
+//
+
+import SwiftUI
+
+struct FeaturedItemView: View {
+  let player: Player
+  
+  var body: some View {
+    Image(player.image)
+      .resizable()
+      .scaledToFit()
+      .cornerRadius(12)
+  }
+}
+
+struct FeaturedItemView_Previews: PreviewProvider {
+  static var previews: some View {
+    FeaturedItemView(player: players[0])
+      .previewLayout(.sizeThatFits)
+      .padding()
+      .background(colorBackground)
+  }
+}
---
+++ Touchdown/View/Home/FeaturedTabView.swift
@@ -0,0 +1,28 @@
+//
+//  FeaturedTabView.swift
+//  Touchdown
+//
+//  Created by Arthur Neves on 01/12/21.
+//
+
+import SwiftUI
+
+struct FeaturedTabView: View {
+  var body: some View {
+    TabView {
+      ForEach(players) { player in
+        FeaturedItemView(player: player)
+          .padding(.top, 10)
+          .padding(.horizontal, 15)
+      }
+    } //: TabView
+    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+  }
+}
+
+struct FeaturedTabView_Previews: PreviewProvider {
+  static var previews: some View {
+    FeaturedTabView()
+      .background(Color.gray)
+  }
+}
